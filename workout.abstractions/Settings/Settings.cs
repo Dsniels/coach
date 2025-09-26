3a236e5d542bc048c836e7642d3cfdbe7be02b14
Daniel Salazar <dsnile.salazr@gmail.com>
2025-09-25 23:17:30 -0600

    add settings

---
+++ workout.abstractions/Settings/Settings.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Configuration;
+
+public class Settings
+{
+    public IConfigurationRoot _config { get; set; }
+    public T GetSettings<T>() => this._config.GetRequiredSection(typeof(T).Name).Get<T>();
+    public OpenAISettings openAISettings => this.GetSettings<OpenAISettings>();
+    public TokenSettings tokenSettings => this.GetSettings<TokenSettings>();
+
+    public class OpenAISettings
+    {
+        public string BaseUrl { get; set; }
+        public string Model { get; set; }
+        public string ApiKey { get; set; }
+    }
+
+    public class TokenSettings
+    {
+        public string Issuer { get; set; }
+        public string Key { get; set; }
+    }
+
+    public Settings()
+    {
+        this._config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                   .AddJsonFile("appsettings.json", false)
+                                   .AddEnvironmentVariables().Build();
+    }
+
+}
